e6b8076c2507cf5c82d56a104b1113be3a7ba0ed
Alexey Nevsky <anwer.man@gmail.com>
2014-01-29 23:27:28 +0300

    1 2 3

---
+++ 2.0/Копия Android/FacemashAndroid/src/com/alexnevsky/FacemashAndroid/utils/Constants.java
@@ -0,0 +1,13 @@
+package com.alexnevsky.FacemashAndroid.utils;
+
+/**
+ * User: Alex Nevsky
+ * Date: 25.10.13
+ */
+public class Constants {
+
+	public static final String SOLT = "NRfd879FTDCe435hvOt0T";
+	public static final String SERVER_UPDATE_FACES_URL = "http://atenwood.com/facemash/updateFacesFromMobile";
+
+	public static final String GOOGLE_URL = "http://google.com";
+}
---
+++ 2.0/Копия Android/FacemashAndroid/src/com/alexnevsky/FacemashAndroid/utils/InternetRequirable.java
@@ -0,0 +1,10 @@
+package com.alexnevsky.FacemashAndroid.utils;
+
+/**
+ * User: Alex Nevsky
+ * Date: 06.11.13
+ */
+public interface InternetRequirable {
+	void checkInternet();
+	void setIsInternet(boolean isInternet);
+}
